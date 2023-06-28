9f033b0a043111aa0a1548edde845f48e2162902
Pasha <you@example.com>
2023-06-28 21:45:50 +0200

    add current index

--- p5_game.js
+++ p5_game.js
@@ -5,23 +5,22 @@ class Box {
         this.width = width;
         this.height = height;
         this.value = value;
-        this.index = index;
+        this.initIndex = index; // initial index, never changes
+        this.currIndex = index; // current index, changes when boxes are swapped
     }
 
     draw() {
         fill(this.value === 1 ? 'FireBrick' : 'ForestGreen');
         rect(this.x, this.y, this.width, this.height);
 
-        // Draw the box index only if showIndexes is true
         if (showIndexes) {
             fill(255);
             textAlign(CENTER, CENTER);
             textSize(18);
-            text(this.index + 1, this.x + this.width / 2, this.y + this.height / 2);
+            text(this.initIndex + 1, this.x + this.width / 2, this.y + this.height / 2);
         }
 
-        // Add dark frame for selected box
-        if (selected !== null && this.index === selected) {
+        if (selected !== null && this.initIndex === selected.initIndex) {
             noFill();
             stroke('black');
             strokeWeight(3);
@@ -35,43 +34,30 @@ class Box {
     }
 
     swap(other) {
-        const tempValue = this.value;
-        const tempIndex = this.index;
+        const tempIndex = this.currIndex;
+        const tempX = this.x;
+        const tempY = this.y;
 
-        this.value = other.value;
-        this.index = other.index;
+        this.currIndex = other.currIndex;
+        this.x = other.x;
+        this.y = other.y;
 
-        other.value = tempValue;
-        other.index = tempIndex;
-    }
-
-    static swapInArray(boxes, a, b) {
-        let temp = boxes[a];
-        console.table(Object.entries(temp));
-        boxes[a] = boxes[b];
-        boxes[b] = temp;
+        other.currIndex = tempIndex;
+        other.x = tempX;
+        other.y = tempY;
     }
 }
 
 let grid = [];
-let targetGrid = []; // Target grid
+let targetGrid = [];
 let selected = null;
 let showIndexes = false;
 
 function setup() {
-    createCanvas(250, 520); // Updated height to accommodate the target grid
+    createCanvas(250, 520);
     let pattern = [1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1];
-    // Calculate the count of 1's in the target pattern
     let countOfOnes = pattern.reduce((count, value) => count + value, 0);
-    // Initialize the grid with Box objects
-    for (let i = 0; i < 16; i++) {
-        const x = (i % 4) * 60 + 10;
-        const y = Math.floor(i / 4) * 60 + 10;
-        const value = i < countOfOnes ? 1 : 0; // First boxes will be 1's
-        grid.push(new Box(x, y + 260, 50, 50, value, i)); // The y-coordinate of the main grid is updated to move it down
-    }
 
-    // Initialize the target grid
     for (let i = 0; i < 16; i++) {
         const x = (i % 4) * 60 + 10;
         const y = Math.floor(i / 4) * 60 + 10;
@@ -79,7 +65,13 @@ function setup() {
         targetGrid.push(new Box(x, y, 50, 50, value, i));
     }
 
-    // Create a button that toggles showIndexes
+    for (let i = 0; i < 16; i++) {
+        const x = (i % 4) * 60 + 10;
+        const y = Math.floor(i / 4) * 60 + 260; // move main grid down
+        const value = i < countOfOnes ? 1 : 0; 
+        grid.push(new Box(x, y, 50, 50, value, i));
+    }
+
     let button = createButton('Toggle Indexes');
     button.mousePressed(() => {
         showIndexes = !showIndexes;
@@ -90,12 +82,10 @@ function setup() {
 function draw() {
     background(220);
 
-    // Draw the target grid
     for (let box of targetGrid) {
         box.draw();
     }
 
-    // Draw the main grid
     for (let box of grid) {
         box.draw();
     }
@@ -105,11 +95,10 @@ function mousePressed() {
     for (let i = 0; i < grid.length; i++) {
         if (grid[i].contains(mouseX, mouseY)) {
             if (selected !== null) {
-                grid[i].swap(grid[selected]);
-                Box.swapInArray(grid, i, selected);
+                grid[i].swap(selected);
                 selected = null;
             } else {
-                selected = i;
+                selected = grid[i];
             }
             redraw();
 
@@ -122,14 +111,10 @@ function mousePressed() {
 }
 
 function checkWin() {
-
-    let gridValues = grid.map(cell => cell.value);
-    let targetGridValues = targetGrid.map(cell => cell.value);
-
-    //console.log(grid[1].value+"(1):"+gridValues.toString()+"===="+targetGridValues.toString()+new Date())
-      if (targetGridValues.toString() == gridValues.toString()) {
-        return false;
-      }
-
-    //return true;
-  }
+    for (let i = 0; i < grid.length; i++) {
+        if (grid[i].value !== targetGrid[grid[i].currIndex].value) {
+            return false;
+        }
+    }
+    return true;
+}
