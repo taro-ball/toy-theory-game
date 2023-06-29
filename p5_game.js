7235c90002dc0fb78fd965f0ce12f03f936a7ca3
Pasha <you@example.com>
2023-06-29 11:54:04 +0200

    introduce createGrid

--- p5_game.js
+++ p5_game.js
@@ -1,41 +1,38 @@
 class Box {
-    constructor(x, y, width, height, value, index, isInteractive) {
+    constructor(x, y, size, value, index, isInteractive) {
         this.x = x;
         this.y = y;
-        this.width = width;
-        this.height = height;
+        this.size = size;
         this.value = value;
         this.initIndex = index; // initial index, never changes
         this.currIndex = index; // current index, changes when boxes are swapped
         this.isInteractive = isInteractive;
     }
 
-
     draw() {
         fill(this.value === 1 ? 'FireBrick' : 'ForestGreen');
-        rect(this.x, this.y, this.width, this.height);
-
+        rect(this.x, this.y, this.size, this.size);
+    
         if (showIndexes) {
             fill(255);
             textAlign(CENTER, CENTER);
             textSize(18);
-            text(this.initIndex + 1, this.x + this.width / 2, this.y + this.height / 2);
+            text(this.initIndex + 1, this.x + this.size / 2, this.y + this.size / 2);
         }
-
+    
         if (this.isInteractive && selected !== null && this.initIndex === selected.initIndex) {
             noFill();
             stroke('black');
             strokeWeight(3);
-            rect(this.x, this.y, this.width, this.height);
+            rect(this.x, this.y, this.size, this.size);
             noStroke();
         }
-
     }
-
+    
     contains(mouseX, mouseY) {
-        return (mouseX > this.x && mouseX < this.x + this.width && mouseY > this.y && mouseY < this.y + this.height);
+        return (mouseX > this.x && mouseX < this.x + this.size && mouseY > this.y && mouseY < this.y + this.size);
     }
-
+    
     swap(other) {
         const tempIndex = this.currIndex;
         const tempX = this.x;
@@ -56,25 +53,28 @@ let targetGrid = [];
 let selected = null;
 let showIndexes = false;
 
+function createGrid(boxSize, isInteractive, topLeftX, topLeftY, pattern) {
+    const grid = [];
+
+    for (let i = 0; i < 16; i++) {
+        const x = topLeftX + (i % 4) * boxSize;
+        const y = topLeftY + Math.floor(i / 4) * boxSize;
+        const value = pattern[i]; 
+        grid.push(new Box(x, y, boxSize, value, i, isInteractive));
+    }
+
+    return grid;
+}
+
 function setup() {
     createCanvas(250, 520);
     let pattern = [1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1];
     let countOfOnes = pattern.reduce((count, value) => count + value, 0);
 
-    for (let i = 0; i < 16; i++) {
-        const x = (i % 4) * 60 + 10;
-        const y = Math.floor(i / 4) * 60 + 10;
-        const value = pattern[i];
-        targetGrid.push(new Box(x, y, 20, 20, value, i, false));
-    }
+    targetGrid = createGrid(20, false, 10, 10, pattern);
 
-    for (let i = 0; i < 16; i++) {
-        const x = (i % 4) * 60 + 10;
-        const y = Math.floor(i / 4) * 60 + 260; // move main workingGrid down
-        const value = i < countOfOnes ? 1 : 0;
-        workingGrid.push(new Box(x, y, 50, 50, value, i, true));
-    }
-
+    let workingPattern = Array(16).fill(0).map((v, i) => i < countOfOnes ? 1 : 0);
+    workingGrid = createGrid(50, true, 10, 260, workingPattern);
 
     let button = createButton('Toggle Indexes');
     button.mousePressed(() => {
@@ -82,6 +82,7 @@ function setup() {
         redraw();
     });
 }
+
 
 function draw() {
     background(220);
