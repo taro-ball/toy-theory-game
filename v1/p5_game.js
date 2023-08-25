652537f0f7a1274e1affc6df37313af5ebd5ad37
Taro B <taro@empyre.tst>
2023-08-25 15:42:43 +0200

    swap-only version

---
+++ v1/p5_game.js
@@ -0,0 +1,401 @@
+const totalBoxes = 16;
+let miniBoxSize = 0;
+let positionXoffset = 0;
+let positionYoffset = 0;
+let riddleSelect;
+let showIndexes = true;
+let logDiv;
+let hintNo = 0;
+let paletteNo = 0;
+let drawArrows = 1;
+
+function setup() {
+    document.addEventListener('contextmenu', event => event.preventDefault());
+    frameRate(5);
+    applyPalette();
+    logDiv = select('#ttLog');
+
+    let canvasContainer = document.getElementById('canvasContainer');
+
+    miniBoxSize = canvasContainer.offsetWidth / 48;
+    let containerWidth = miniBoxSize * 46;
+    let containerHeight = miniBoxSize * 34;//canvasContainer.offsetHeight;
+
+    positionXoffset = miniBoxSize;
+    positionYoffset = miniBoxSize;
+
+    let myCanvas = createCanvas(containerWidth, containerHeight);
+
+    //let myCanvas = createCanvas(miniBoxSize * 46, miniBoxSize * 34);
+    //let myCanvas = createCanvas();
+    myCanvas.parent('canvasContainer');
+    myCanvas.color = "red";
+
+    let row1 = select('#row1');
+    let row2 = select('#row2');
+
+    riddleSelect = createSelect();
+    riddleSelect.parent(row1);
+    //riddleSelect.position(windowWidth / 2 + 90, 130);
+    for (let i = 0; i < riddles.length; i++) {
+        riddleSelect.option('Riddle ' + (i + 1), i);
+    }
+    riddleSelect.changed(redrawSketch);
+
+    let resetButton = createButton('Reset');
+    resetButton.parent(row1);
+    resetButton.mousePressed(redrawSketch);
+
+    let isMapApplied = false;
+    let applyMapButton = createButton('Apply Map');
+    applyMapButton.parent(row1);
+    //applyMapButton.position(windowWidth / 2 + 90, 105);
+    applyMapButton.mousePressed(() => {
+        let map = getMapFromGrid(workingGrid);
+        if (isMapApplied) {
+            map = inverseMapping(map);
+            applyMapButton.html('Apply Map'); // Change button text back to 'Apply Map'
+        } else {
+            applyMapButton.html('Undo Map'); // Change button text to 'Undo Map'
+        }
+        for (let i = 0; i < sourceGrids.length; i++) {
+            try {
+                applyMapping(sourceGrids[i], map);
+            } catch (error) {
+                alert('Invalid mapping. Please enter a valid map.');
+                return;
+            }
+        }
+        isMapApplied = !isMapApplied;
+        redraw();
+    });
+
+    let arrowsButton = createButton('Toggle Arrows');
+    arrowsButton.parent(row2);
+    arrowsButton.mousePressed(() => {
+        drawArrows = !drawArrows;
+        redraw();
+    });
+
+    let hintButton = createButton('Hint');
+    hintButton.parent(row1);
+    hintButton.mousePressed(() => { gameLog(`<b>Hint:</b> ${riddleHint()}`) });
+
+    let paletteButton = createButton('Color');
+    paletteButton.parent(row2);
+    paletteButton.mousePressed(() => {
+        gameLog(`Applying palette "${gamePalettes[paletteNo].name}"...`);
+        applyPalette();
+    });
+
+
+    let helpButton = createButton('<b>Help</b>');
+    helpButton.parent(row1);
+    helpButton.mousePressed(() => { gameLog(helpMessage[0]) });
+
+    // Draw initial board
+    gameLog(greetMessage);
+    redrawSketch();
+}
+
+class Box {
+    constructor(x, y, boxSize, value, index, isInteractive, boxType) {
+        this.x = x;
+        this.y = y;
+        this.boxSize = boxSize;
+        this.value = value;
+        this.initIndex = index;
+        this.currIndex = index;
+        this.isInteractive = isInteractive;
+        this.flash = false;
+        this.isLocked = false;
+        this.boxType = boxType;
+    }
+
+    drawBox() {
+        stroke(this.flash ? colorTXT : colorStroke);
+        strokeWeight(this.flash ? miniBoxSize / 2 : miniBoxSize / 16);
+        fill(this.value === 1 ? (this.boxType === 1 ? color2 : color1) : color0);
+        rect(this.x, this.y, this.boxSize, this.boxSize);
+    }
+
+    drawIndex() {
+        fill(colorTXT);
+        textAlign(CENTER, CENTER);
+        textSize(this.boxSize / 2)
+        noStroke(); // Removes stroke from text
+        let indexTxt = this.isLocked ? '[' + this.initIndex + ']' : this.initIndex;
+        text(indexTxt, this.x + this.boxSize / 2, this.y + this.boxSize / 2);
+        stroke('black');
+    }
+
+    drawSelected() {
+        if (!this.isLocked) {
+            fill(colorSelect);
+            noStroke();
+            rect(this.x, this.y, this.boxSize, this.boxSize);
+        }
+    }
+
+    drawArrow() {
+        //let arrowDistance = 6; // Distance between arrows
+        let xoffset = miniBoxSize;
+        let yoffset = this.boxSize * 4.3;
+        //gameLog(yoffset);
+        if (this.initIndex !== this.currIndex && drawArrows) {
+            const startX = workingGrid[this.currIndex].getPosition().x + this.boxSize / 2; // this.initIndex % 4 * this.boxSize + xoffset + this.boxSize / 2;
+            const startY = workingGrid[this.currIndex].getPosition().y + this.boxSize / 2; //Math.floor(this.initIndex / 4) * this.boxSize + yoffset + this.boxSize / 2;
+            //gameLog(workingGrid[this.initIndex].getPosition()[0]) ;
+            const endX = this.x + this.boxSize / 2; //this.currIndex % 4 * this.boxSize + xoffset + this.boxSize / 2;
+            const endY = this.y + this.boxSize / 2; //Math.floor(this.currIndex / 4) * this.boxSize + yoffset + this.boxSize / 2;
+
+            // Calculate the angle
+            const arrowAngle = Math.atan2(endY - startY, endX - startX);
+
+            // Calculate arrow head points
+            const arrowLength = miniBoxSize;
+            const arrowWidth = miniBoxSize / 3;
+            const arrowHeadX1 = endX - arrowLength * Math.cos(arrowAngle) + arrowWidth * Math.sin(arrowAngle);
+            const arrowHeadY1 = endY - arrowLength * Math.sin(arrowAngle) - arrowWidth * Math.cos(arrowAngle);
+            const arrowHeadX2 = endX - arrowLength * Math.cos(arrowAngle) - arrowWidth * Math.sin(arrowAngle);
+            const arrowHeadY2 = endY - arrowLength * Math.sin(arrowAngle) + arrowWidth * Math.cos(arrowAngle);
+
+            stroke(color1);
+            strokeWeight(miniBoxSize / 8);
+            line(startX, startY, endX, endY);
+            line(arrowHeadX1, arrowHeadY1, endX, endY);
+            line(arrowHeadX2, arrowHeadY2, endX, endY);
+            noStroke();
+        }
+    }
+
+
+
+    draw() {
+        this.drawBox();
+        if (this.isInteractive) {
+            if (selected !== null && this.initIndex === selected.initIndex && !this.isLocked) { // Check for locked status here
+                this.drawSelected();
+            }
+            if (showIndexes) {
+                this.drawIndex()
+            };
+            //this.drawArrow();
+        }
+
+    }
+
+    setPosition(x, y, index) {
+        this.x = x;
+        this.y = y;
+        this.currIndex = index;
+    }
+
+    getPosition() {
+        return { "x": this.x, "y": this.y };
+    }
+
+    swap(other) {
+        if (this.isLocked || other.isLocked) { // If either box is locked, prevent swap
+            return;
+        }
+
+        [this.x, other.x] = [other.x, this.x];
+        [this.y, other.y] = [other.y, this.y];
+        [this.currIndex, other.currIndex] = [other.currIndex, this.currIndex];
+
+        this.flash = true;
+        other.flash = true;
+
+
+        //mapInput.value(getMapFromGrid(workingGrid).join(','));
+
+        setTimeout(() => {
+            this.flash = false;
+            other.flash = false;
+
+            redraw();
+        }, 100);
+    }
+
+    contains(mouseX, mouseY) {
+        return (mouseX > this.x && mouseX < this.x + this.boxSize && mouseY > this.y && mouseY < this.y + this.boxSize);
+    }
+
+    toggleLock() {
+        this.isLocked = !this.isLocked; // Toggle the lock status
+    }
+}
+
+
+function redrawSketch() {
+    bgcolor1 = 220;
+
+    riddleIndex = parseInt(riddleSelect.value());
+    myPatterns = riddles[riddleIndex].boardPatterns;
+    currentTargetMap = riddles[riddleIndex].targetMap;
+
+    sourceGrids = [];
+    targetGrids = [];
+    workingGrid = [];
+    selected = null;
+    hintNo = 0;
+    lockedStrokeW = 6;
+
+    for (let i = 0; i < myPatterns.length; i++) {
+        let pattern = myPatterns[i].map(num => num); // Copy each pattern into a separate array
+        let boxSize = miniBoxSize;
+        let positionX = positionXoffset + i * miniBoxSize * 5;
+        let positionY = positionYoffset;
+        sourceGrids.push(createGrid(boxSize, false, positionX, positionY, pattern, 0));
+    }
+
+    for (let i = 0; i < myPatterns.length; i++) {
+        let pattern = myPatterns[i].map(num => num); // Copy each pattern into a separate array
+        let boxSize = miniBoxSize;
+        let positionX = positionXoffset + i * miniBoxSize * 5;
+        let positionY = positionYoffset + miniBoxSize * 5.5;
+        targetGrids.push(createGrid(boxSize, false, positionX, positionY, pattern, 1));
+    }
+
+    for (let i = 0; i < targetGrids.length; i++) {
+        try {
+            applyMapping(targetGrids[i], currentTargetMap);
+        } catch (error) {
+            alert('Invalid mapping. Please enter a valid map.');
+            return;
+        }
+    }
+
+    workingGrid = createGrid(miniBoxSize * 4, true, positionXoffset, positionYoffset + miniBoxSize * 16, Array(totalBoxes).fill(0), 0);
+    //mapInput.value(getMapFromGrid(workingGrid).join(','));
+    gameLog(`<hr><b>Riddle</b> "${riddles[riddleIndex].name}" by ${riddles[riddleIndex].author} <b>loaded!</b>`)
+    redraw();
+}
+
+
+function createGrid(boxSize, isInteractive, topLeftX, topLeftY, pattern, boxType) {
+    const grid = [];
+
+    for (let i = 0; i < totalBoxes; i++) {
+        const x = topLeftX + (i % 4) * boxSize;
+        const y = topLeftY + Math.floor(i / 4) * boxSize;
+        const value = pattern[i];
+        grid.push(new Box(x, y, boxSize, value, i, isInteractive, boxType));
+    }
+
+    return grid;
+}
+
+function getMapFromGrid(grid) {
+    let map = new Array(grid.length);
+    for (let i = 0; i < grid.length; i++) {
+        map[grid[i].currIndex] = i;
+    }
+    return map;
+}
+
+function applyMapping(grid, map) {
+    // Ensure map is a permutation of numbers from 0 to 15
+    const mapCheck = [...map].sort((a, b) => a - b);
+    for (let i = 0; i < mapCheck.length; i++) {
+        if (mapCheck[i] !== i) {
+            throw new Error('Invalid mapping');
+        }
+    }
+
+    // Store the original positions
+    let originalPositions = grid.map(box => ({ x: box.x, y: box.y }));
+
+    for (let i = 0; i < totalBoxes; i++) {
+        const mappedIndex = map[i];
+        grid[mappedIndex].x = originalPositions[i].x;
+        grid[mappedIndex].y = originalPositions[i].y;
+        grid[mappedIndex].currIndex = i;
+    }
+}
+
+function drawGrid(grid) {
+    for (let box of grid) {
+        box.draw();
+    }
+}
+
+function draw() {
+    background(bgcolor1);
+    sourceGrids.forEach(drawGrid);
+    targetGrids.forEach(drawGrid);
+    drawGrid(workingGrid);
+
+    // Draw arrows after all boxes have been drawn
+    for (let box of workingGrid) {
+        box.drawArrow();
+    }
+
+    strokeWeight(miniBoxSize / 8);
+    for (let i = 0; i < myPatterns.length; i++) {
+        stroke(color2);
+        drawDecor(miniBoxSize * 3 + i * miniBoxSize * 5, positionYoffset + miniBoxSize * 4.15, miniBoxSize);
+        stroke(color1);
+        drawDecor(miniBoxSize * 3 + i * miniBoxSize * 5, positionYoffset - miniBoxSize / 3 + miniBoxSize * 4.15, miniBoxSize);
+    }
+    noStroke();
+}
+
+function interact() {
+    if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
+        for (let i = 0; i < workingGrid.length; i++) {
+            if (workingGrid[i].contains(mouseX, mouseY) && workingGrid[i].isInteractive) {
+                if (mouseButton === LEFT) {
+                    if (selected !== null) {
+                        workingGrid[i].swap(selected);
+                        selected = null;
+                    } else {
+                        selected = workingGrid[i];
+                    }
+                    redraw();
+
+                    if (checkWin()) {
+                        winMsg = riddles[riddleIndex].CustomWinMessage ?? winMessages[Math.floor(Math.random() * winMessages.length)];
+                        bgcolor1 = color1;
+                        // textAlign(CENTER, CENTER);
+                        // textSize(440)
+                        // fill(colorTXT)
+                        // text("WIN!!!", miniBoxSize, miniBoxSize);
+                        gameLog(`<H1>WIN!!!</H1><H3> ${winMsg} </H3>`);
+                    }
+                    break;
+                } else if (mouseButton === RIGHT) {
+                    workingGrid[i].toggleLock();
+                    redraw();
+                    break;
+                }
+            }
+        }
+    }
+}
+
+
+
+function mousePressed() {
+    interact();
+}
+// keep it commented until figure out touch controls
+// function touchStarted() {
+//     interact();
+//     return false; // Prevent default behavior
+// }
+
+function checkWin() {
+    let workingGridMap = workingGrid.map(grid => grid.currIndex);
+    //gameLog('Current mapping: ' + workingGridMap.toString());
+    //gameLog('currentTargetMap: ', currentTargetMap);
+    for (let i = 0; i < workingGridMap.length; i++) {
+        if (workingGridMap[i] !== currentTargetMap[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
---
+++ v1/p5_patterns.js
@@ -0,0 +1,174 @@
+function riddleHint() {
+    let hints = riddles[riddleIndex].hints;
+    if (!hints || hints.length === 0) {
+        return "Sorry, <b>no hints</b> here. You are on your own!";
+    }
+
+    let hint = hints[hintNo];
+    hintNo = (hintNo + 1) % hints.length; // Increment hint index and reset to 0 after reaching the last hint
+
+    return hint;
+}
+
+function gameLog(message) {
+    let previousHtml = logDiv.html(); // get the current log
+    logDiv.html(previousHtml + message + '<br><br>'); // append the new message and a newline
+    logDiv.elt.scrollTop = logDiv.elt.scrollHeight; // scroll to the bottom
+}
+
+function applyPalette() {
+    color0 = gamePalettes[paletteNo].color0;
+    color1 = gamePalettes[paletteNo].color1;
+    color2 = gamePalettes[paletteNo].color2 ? gamePalettes[paletteNo].color2 : gamePalettes[paletteNo].color1;
+    colorStroke = gamePalettes[paletteNo].colorStroke;
+    colorTXT = gamePalettes[paletteNo].colorTXT;
+    colorSelect = gamePalettes[paletteNo].colorSelect;
+
+    paletteNo = (paletteNo + 1) % gamePalettes.length; // Increment index and reset to 0 after reaching the last one
+}
+
+function inverseMapping(map) {
+    let inverse = new Array(map.length);
+    for (let i = 0; i < map.length; i++) {
+        inverse[map[i]] = i;
+    }
+    return inverse;
+}
+
+function drawDecor(x, y, size) {
+    line(x - size / 2, y + size / 2, x, y + size);
+    line(x + size / 2, y + size / 2, x, y + size);
+}
+
+greetMessage = [`<hr>📓Based on:<br> <a href=https://en.wikipedia.org/wiki/Spekkens_toy_model>Rob Spekkens's toy model</a><br>
+🧩github.com:<br> <a href=https://github.com/taro-ball/toy-theory-game>toy-theory-game</a>`]
+
+helpMessage = [`<b>How to play:</b>
+
+#️⃣ Use the grid at the bottom to present your answer.
+
+🕹️ Select a tile by clicking on it, then choose another tile to swap with. Repeat until you have arranged the tiles to match your guess.\
+If you are correct, you will receive a win message. If not, you can reset the arrangement and try again. \
+To view the result of your guess, click "Apply Map" Clicking 'Apply Map' again will reveal the original question.`]
+
+winMessages = [
+    "You're a mastermind, a puzzle-solving wizard! 🔮🧩 Take a bow as the crowd goes wild and unicorns rain from the virtual sky! 🦄🌈",
+    "Congratulations! You've unscrambled this puzzle faster than light! 🌟 But remember, according to Einstein, time is relative! 🕓⚛️",
+    "Eureka! Archimedes would be proud of you. You've cracked the puzzle and upheld the principle of buoyancy! 🛁👏",
+    "You've solved it, proving that the sum of your IQ is greater than ∞! ➕🧠=∞",
+    "Puzzle: Solved! 🎉 You've brought order to this chaotic system. Take that, entropy! 🌌↩️",
+    "You've conquered this puzzle like Newton conquering gravity! 🍏👑 Don't let the apple fall far from your wisdom tree!",
+    "Well done! You've disproved uncertainty by locating both the puzzle's place and momentum! 🌠💡 Heisenberg would be perplexed!",
+    "You've pieced together this puzzle like Schrödinger's cat in a superposition! 🐱📦🎉 Still alive and kicking!",
+    "Well done! You've traversed through this maze like a photon in a double-slit experiment! 🎇💡 Truly enlightening!",
+    "Incredible! You've tackled this puzzle like a Pythagorean theorem— a² + b² always equals victory²! 🎉📐",
+    "You've broken through the puzzle barrier faster than the speed of sound! 🎊🔊 Mach you look good!",
+    "Hats off to you! 🎩 You've vanquished this puzzle like Zeno's paradox— infinitely overcoming the halves to reach the goal! 🏁🐢",
+    "Hurrah! You've filled this puzzle's gaps faster than a mathematician fills a number line! 📈➖➕=🥳",
+    "You've solved this puzzle smoother than a perfectly rendered Bezier curve! 📈🎊 Designing success!",
+    "Astonishing! You've pieced together this puzzle faster than Boltzmann distributing particles in a gas! ⚛️🎈",
+    "Your resolution to this puzzle is more elegant than Euler's identity. e^(iπ)+1=0 now equals victory! 🎓🏅",
+    "You've connected the puzzle pieces faster than the speed of light in a vacuum. c 🎉🔦 Here's to breaking no laws!",
+    "Incredible! You've traversed the dimensions of this puzzle like a string theorist through multiverses! 🪐🎻",
+    "Just as Fermat's Last Theorem stood no chance against Wiles, this puzzle stood no chance against you! 🎊🧮",
+    "Well done! You've demonstrated the principle of causality by causing this puzzle to fall into place! 🪐➡️💫",
+    "Puzzle: Solved! You've zeroed in on the solution as precisely as an atom's half-life in a radioactive sample! ☢️➡️⏳",
+    "Stupendous! You've observed the puzzle solution, just like Bohr's model observing the quantum leaps of an electron! 🎊⚡️"
+]
+
+gamePalettes = [{
+    "name": "Light",
+    "color0": "White",
+    "color1": "CornflowerBlue",
+    "color2": "LightCoral",
+    "colorStroke": "Grey",
+    "colorSelect": "LightCoral",
+    "colorTXT": "Grey"
+}, {
+    "name": "Original",
+    "color0": "DarkTurquoise",
+    "color1": "Purple",
+    "colorStroke": "Grey",
+    "colorSelect": "MediumOrchid",
+    "colorTXT": "White"
+}, {
+    "name": "Khaki",
+    "color0": "#9dad7f",
+    "color1": "#557174",
+    "colorStroke": "#525e75",
+    "colorSelect": "#c7cfb7",
+    "colorTXT": "#f7f7e8"
+}, {
+    "name": "Crayon",
+    "color0": "#b6e2d3",
+    "color1": "#ef7c8e",
+    "colorStroke": "DarkGrey",
+    "colorSelect": "#fae8e0",
+    "colorTXT": "#d8a7b1"
+}, {
+    "name": "Violet",
+    "color0": "#8e98f5",
+    "color1": "DarkViolet",
+    "colorStroke": "#b1cbfa",
+    "colorSelect": "#b1cbfa",
+    "colorTXT": "#dfe2fe"
+}]
+
+riddles = [{
+    "targetMap": [2, 7, 0, 5, 6, 3, 4, 1, 8, 13, 10, 15, 12, 9, 14, 11],
+    "boardPatterns": [
+        [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
+        [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1],
+        [0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0],
+        [1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0],
+        [0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0],
+        [0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0]
+    ],
+    "name": "The Original🧩",
+    "author": "Rob🧑‍🏫",
+    "CustomWinMessage": "Impressive! You cracked it! 🥳🍾 (but Nitica was the first 😏🥇)",
+}, {
+    "targetMap": [0, 1, 2, 11, 4, 5, 6, 15, 8, 9, 10, 3, 12, 13, 14, 7],
+    "boardPatterns": [
+        [1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1],
+        [1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1],
+        [1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0],
+        [1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1]
+    ],
+    "name": "The sweet one 🍭",
+    "author": "Baker🧑‍🍳",
+    "hints": ["Only involves tiles in the last column, but which ones?😏", "Ok, the top one is 11🦄.", "Still did not guess? Second from the top - 15🙄"]
+},
+{
+    "targetMap": [0, 1, 2, 3, 4, 5, 10, 7, 8, 9, 6, 11, 12, 13, 14, 15],
+    "boardPatterns": [
+        [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1],
+        [0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0],
+        [1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1],
+        [0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0],
+        [1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1],
+        [0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0],
+        [1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1]
+    ],
+    "name": "Awesomeness of geometry📐",
+    "author": "Robot🤖",
+    "CustomWinMessage": "I am robot, you can not win me! Beep-beep-bup!",
+},
+{
+    "targetMap": [15, 1, 2, 3, 4, 5, 10, 7, 8, 9, 6, 11, 12, 13, 14, 0],
+    "boardPatterns": [
+        [0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0],
+        [0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1],
+        [0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0]
+    ],
+    "name": "Greener pastures 🌾🌾🌾",
+    "author": "Bor🔰"
+}
+
+]
